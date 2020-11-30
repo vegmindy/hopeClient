e2fc5a6dea447f1b0f4cc9dbd09259fcac126a7b
Josh Mason <jamason87@gmail.com>
2020-11-30 15:09:56 -0500

    Search Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import {useState} from 'react';
 import Review from "./Components/Review/Review"
 import Auth from './Components/Auth/Auth';
-import Favorites from './Components/Favorites/Favorites'
 import './App.css';
-
-
+import Search from "./Components/Search/Search"
 
 function App() {
   const [token, setToken] = useState(undefined);
 
   const viewConductor = () => {
-    return (token === undefined) ? <Auth updateToken={setToken} /> : <Favorites token={token}/>;
+    return (token === undefined) ? <Auth updateToken={setToken} /> : <Review token={token}/>;
   }
 
   return (
     <div className="App">
       {/* <Navbar /> */}
        <Search />
-      {viewConductor()}
+      {/* {viewConductor()} */}
     </div>
   );
 }
--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,34 +1,213 @@
-import {useState, useEffect} from 'react';
-import SearchResults from './SearchResults/SearchResults';
-import { useState, useEffect } from "react";
+import {useState, useEffect, Component} from 'react';
+import {Container, Row, Col, Button, Input, InputGroup, InputGroupAddon, Modal, ModalHeader, ModalFooter, ModalBody} from 'reactstrap';
+import './Search.css';
 
-//914eb4182d2c499cb87ff5c6155012a2 api key
-//http example  https://api.rawg.io/api/games?key=YOUR_API_KEY&dates=2019-09-01,2019-09-30&platforms=18,1,7
-const Search = () => {
+class Search extends Component {
+    constructor(props) {
+        super(props);
 
-    const [results, setResults] = useState([])
+        this.state = {
+            page: 1,
+            resultList: [],
+            searchValue: "",
+            gameModal: {
+                show: false,
+                name: "Test Theft Cycle",
+                desc: "Test Theft Cycle is the next installment in non motorized pedestrian vechile theft game genre.  This will really knock your chains off."
+            }
+        }
 
-    useEffect(() => {
-        fetch("https://api.rawg.io/api/games?914eb4182d2c499cb87ff5c6155012a2", {
+        this.handleSearchChange = this.handleSearchChange.bind(this);
+        this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
+        this.toggleGameDetailsModal = this.toggleGameDetailsModal.bind(this);
+    }
+
+    defaultLoad() {  //Called after page is loaded.
+        this.loadPage();
+    }
+
+    handleSearchChange(event) { //Called everytime you type a character into search bar
+        this.setState({
+            searchValue: event.target.value
+        })
+    }
+
+    handleSearchSubmit(event) { //Called when you hit the search button
+        this.searchGame(1);
+
+        event.preventDefault();
+    }
+
+    searchGame(pageNum) { //Used to lookup the search from our server and returns the resutls
+        let body = {
+            page: pageNum,
+            searchQuery: this.state.searchValue
+        }
+
+        fetch(`http://localhost:4001/search/`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data.results);
+                this.setState({
+                    resultList: data.results.map((item) => { return {name: item.name, backgroundImage: item.background_image, gameId: item.id}})
+                });
+            })
+            .catch(error => console.log(error));
+    }
+
+    loadPage() { //Loads a specific page, depending on search or just default load
+        let pageNum = this.state.page;
+
+        console.log(pageNum)
+
+        if (this.state.searchValue !== "") {
+            this.searchGame(pageNum);
+            return;
+        }
+
+        fetch(`http://localhost:4001/search/page/${pageNum}`, {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
             }
         })
             .then(res => res.json())
-            .then(data => setResults(data.results))
-    }, [])
-
-    return (
-            results.map((result, index) => {
-                return (
-                    <div>
-                    <p>{result.name}</p>
-                    </div>
-                    )
-
+            .then(data => {
+                console.log(data.results)
+                this.setState({
+                    resultList: data.results.map((item) => { return {name: item.name, backgroundImage: item.background_image, gameId: item.id}})
+                });
             })
-    )
+            .catch(error => console.log(error));
+    }
+
+    changePage(amount) { //changes the page by a specific amount
+        let newPage = this.state.page + amount;
+
+        console.log(newPage)
+
+        if (newPage < 1) {
+            this.setState({
+                page: 1
+            }, () => {this.loadPage()})
+        } else {
+            this.setState({
+                page: newPage
+            }, () => {this.loadPage()})
+        }
+    }
+
+    showGameDetails(id) {
+        let tmpGameModal = this.state.gameModal;
+
+        fetch(`http://localhost:4001/search/game/${id}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data) //SHOWS US DATA FROM THE GAME WE CLICK ON
+
+                let gameObj = {
+                    name: data.name,
+                    desc: data.description_raw
+                }
+
+                let tmpGameModal = this.state.gameModal;
+
+                tmpGameModal.name = gameObj.name;
+                tmpGameModal.desc = gameObj.desc;
+
+                this.setState({ gameModal: tmpGameModal });
+
+                this.toggleGameDetailsModal();
+            })
+            .catch(error => console.log(error));
+    }
+
+    toggleGameDetailsModal() {
+        let tmpGameModal = this.state.gameModal;
+
+        tmpGameModal.show = !this.state.gameModal.show;
+
+        this.setState({ gameModal: tmpGameModal});
+    }
+
+    componentDidMount() {
+        this.defaultLoad();
+    }
+
+    render() {
+        return (
+            <div>
+                <div style={{height: '50px'}}>
+                    <div style={{backgroundColor: "white"}} className="fixed-top">
+                        <Container>
+                            <Row>
+                                <Col>
+                                    <InputGroup>
+                                        <Input style={{height: "auto"}} type="text" value={this.state.searchValue} onChange={this.handleSearchChange} placeholder="Search..." />
+                                        <InputGroupAddon addonType="append">
+                                            <Button onClick={this.handleSearchSubmit} type="button">Submit</Button>
+                                        </InputGroupAddon>
+                                    </InputGroup>
+                                </Col>
+                            </Row>
+                        </Container>
+                    </div>
+                </div>
+                <Container style={{backgroundColor: "white"}}>
+                    {
+                        this.state.resultList.map((item) => {
+                            return <Row>
+                                <Col onClick={() => {this.showGameDetails(item.gameId)}}>
+                                    <div className="gameContainer" style={{
+                                            backgroundImage: (item.backgroundImage !== null)
+                                             ? "url('" + item.backgroundImage +"')" : "linear-gradient(135deg, #231437 0%, #2c385e 50%, #336e6b 100%)"
+                                        }}>
+
+                                        <div className="gameTitle">{item.name}</div>
+                                    </div>
+                                </Col>
+                            </Row>
+                        })
+                    }
+                </Container>
+                <div>
+                    <div style={{height: '50px'}}>
+                        <div className="fixed-bottom">
+                            <Container>
+                                <Row style={{backgroundColor: "white"}}>
+                                    <Col>
+                                        <Button className="float-left" onClick={() => {this.changePage(-1)}} color="primary">Previous</Button>
+                                    </Col>
+                                    <Col>
+                                        <Button className="float-right" onClick={() => {this.changePage(1)}} color="primary">Next</Button>
+                                    </Col>
+                                </Row>
+                            </Container>
+                        </div>
+                    </div>
+                </div>
+                <Container><Modal size="lg" isOpen={this.state.gameModal.show} toggle={this.toggleGameDetailsModal} >
+                    <ModalHeader toggle={this.toggleGameDetailsModal}>{this.state.gameModal.name}</ModalHeader>
+                        <ModalBody>
+                            {this.state.gameModal.desc}
+                        </ModalBody>
+                    </Modal>
+                </Container>
+                
+            </div>
+        );
+    }
 }
 
-// export default Search;
+export default Search;
