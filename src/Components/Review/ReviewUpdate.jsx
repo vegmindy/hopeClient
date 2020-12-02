7abdf21bda8d7a583fac06e6131cb49d4857020a
vegmindy <72030094+vegmindy@users.noreply.github.com>
2020-12-02 17:58:53 -0500

    updated update

--- src/Components/Review/ReviewUpdate.jsx
+++ src/Components/Review/ReviewUpdate.jsx
@@ -1,11 +1,14 @@
 import React, {useState} from 'react';
 import {Button, Form, FormGroup, Label, Input, Modal, ModalHeader, ModalBody} from 'reactstrap';
+import TokenContext from '../../Contexts/TokenContext'
 import APIURL from '../../helpers/environment'
 
 const ReviewEdit = (props) => {
+    let tokenthing = React.useContext(TokenContext)
+    let token = tokenthing.token
 
-    const [editRating, setEditRating] = useState(0);
-    const [editReview, setEditReview] = useState("");
+    const [editRating, setEditRating] = useState(props.location.state.userRating);
+    const [editReview, setEditReview] = useState(props.location.state.userReview);
 
     const reviewUpdate = (event, review) => {
         event.preventDefault();
