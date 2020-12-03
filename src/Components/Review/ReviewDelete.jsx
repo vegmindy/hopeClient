335ebf75e85c53076312abd2c8e23d2b2d9e8b2b
vegmindy <72030094+vegmindy@users.noreply.github.com>
2020-12-03 08:38:27 -0500

    updated delete

--- src/Components/Review/ReviewDelete.jsx
+++ src/Components/Review/ReviewDelete.jsx
@@ -1,34 +1,44 @@
 import React, {useState} from 'react';
-import {Button, Form, FormGroup, Label, Input, Modal, ModalHeader, ModalBody, Container} from 'reactstrap';
+import {Button, Form, FormGroup, Label, Input, CloseButton, Modal, ModalHeader, ModalBody, Container} from 'reactstrap';
 import TokenContext from '../../Contexts/TokenContext';
 import APIURL from '../../helpers/environment'
 import { Link } from 'react-router-dom';
 
 
 const deleteReview = (props) => {
+    // let reviewID= props.location.state.reviewID
+    let jsonBody = {
+        id: props.location.state.reviewID
+    }
+    function deleteUserReview(e){
+        e.preventDefault();
+    fetch(`${APIURL}/review/delete/`, {
+        method: 'DELETE',
+        headers:{
+            'Content-Type': 'application/json',
+            'Authorization': props.token
+        }, 
+        body: JSON.stringify(jsonBody)
+    })
+    .then((res) => res.json)
+    }
 
-    // let token = React.useContext(TokenContext)
-        fetch(`${APIURL}/review/${props.location.state.reviewID}`, {
-            method: 'DELETE',
-            headers:({
-                'Content-Type': 'application/json',
-                 'Authorization': props.token
-            })
-        })
-        .then((res) => res.json())
     
+    
+
         return (
             <div>
                 <Modal isOpen={true}>
                 <ModalHeader>Are you sure you'd like to delete?</ModalHeader>
+                {/* <ModalHeader closeButton>
+                    <Modal.Title>Are you sure you'd like to delete?</Modal.Title>
+                </ModalHeader> */}
                 <ModalBody>
                     <p>Are you sure you want to delete the entry for {props.location.state.gameTitle}? If yes, click delete. If no, click return to Reviews</p>
                     <Link to="/review/all" style={{color: 'white'}}>
                     <Button color="dark"><Link to="/review/all" style={{color: 'white'}}>Return to my reviews</Link></Button>  
                     </Link>
-                    <Link to="/review/all" style={{color: 'white'}}>
-                    <Button color="dark" onSubmit={deleteReview}>Delete</Button>  
-                    </Link>
+                    <Button color="dark" onClick={deleteUserReview}>Delete</Button>
                 </ModalBody>
                 </Modal>
             </div>
