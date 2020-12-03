ab18a58efb9f762714c9f98c035072334f63f011
vegmindy <72030094+vegmindy@users.noreply.github.com>
2020-12-02 19:23:59 -0500

    changed format of update

--- src/Components/Review/ReviewUpdate.jsx
+++ src/Components/Review/ReviewUpdate.jsx
@@ -1,51 +1,50 @@
-import React, {useState} from 'react';
-import {Button, Form, FormGroup, Label, Input, Modal, ModalHeader, ModalBody} from 'reactstrap';
-import TokenContext from '../../Contexts/TokenContext'
-import APIURL from '../../helpers/environment'
+// import React, {useState} from 'react';
+// import {Button, Form, FormGroup, Label, Input, Modal, ModalHeader, ModalBody} from 'reactstrap';
+// import TokenContext from '../../Contexts/TokenContext'
+// import APIURL from '../../helpers/environment'
 
-const ReviewEdit = (props) => {
-    let tokenthing = React.useContext(TokenContext)
-    let token = tokenthing.token
+// const ReviewEdit = (props) => {
+//     let tokenContext = React.useContext(TokenContext);
 
-    const [editRating, setEditRating] = useState(props.location.state.userRating);
-    const [editReview, setEditReview] = useState(props.location.state.userReview);
+//     const [editRating, setEditRating] = useState(props.location.state.userRating);
+//     const [editReview, setEditReview] = useState(props.location.state.userReview);
 
-    const reviewUpdate = (event, review) => {
-        event.preventDefault();
-        fetch(`${APIURL}/review/update${props.reviewToUpdate.id}`,{
-            method: 'PUT',
-            body: JSON.stringify({log: {rating: editRating, review: editReview}}),
-            headers: new Headers({
-                'Content-Type': 'application/json',
-                'Authorization': props.token
-            })
-        }) .then((res) => {
-            props.fetchReviews();
-            props.updateOff();
-        })
-    }
-    return(
-        <Modal isOpen={true}>
-            <ModalHeader>Edit a Review</ModalHeader>
-            <ModalBody>
-                <Form onSubmit={reviewUpdate}>
-                    <FormGroup>
-                        <Label htmlFor="rating">Edit Rating:</Label>
-                        <Input name="rating" value={editRating} onChange={(e)=> setEditRating(e.target.value)}/>
-                    </FormGroup>
-                    <FormGroup>
-                        <Label htmlFor="review">Edit Review:</Label>
-                        <Input name="review" value={editReview} onChange={(e) => setEditReview(e.target.value)}/>
-                    </FormGroup>
-                    <Button type="submit">Update Review</Button>
-                </Form>
-            </ModalBody>
-        </Modal>
-    )
-}
+//     const reviewUpdate = (event, review) => {
+//         event.preventDefault();
+//         fetch(`${APIURL}/review/update${props.reviewToUpdate.id}`,{
+//             method: 'PUT',
+//             body: JSON.stringify({update: {rating: editRating, review: editReview}}),
+//             headers: new Headers({
+//                 'Content-Type': 'application/json',
+//                 'Authorization': TokenContext.token
+//             })
+//         }) .then((res) => {
+//             props.fetchReviews();
+//             props.updateOff();
+//         })
+//     }
+//     return(
+//         <Modal isOpen={true}>
+//             <ModalHeader>Edit a Review</ModalHeader>
+//             <ModalBody>
+//                 <Form onSubmit={reviewUpdate}>
+//                     <FormGroup>
+//                         <Label htmlFor="rating">Edit Rating:</Label>
+//                         <Input name="rating" value={editRating} onChange={(e)=> setEditRating(e.target.value)}/>
+//                     </FormGroup>
+//                     <FormGroup>
+//                         <Label htmlFor="review">Edit Review:</Label>
+//                         <Input name="review" value={editReview} onChange={(e) => setEditReview(e.target.value)}/>
+//                     </FormGroup>
+//                     <Button type="submit">Update Review</Button>
+//                 </Form>
+//             </ModalBody>
+//         </Modal>
+//     )
+// }
 
 
-export default ReviewEdit;
+// export default ReviewEdit;
 
 
 
@@ -74,3 +73,70 @@ export default ReviewEdit;
 // export default UpdateReview;
 
 
+import React, {useState} from 'react';
+import {Button, Form, FormGroup, Label, Input, Modal, ModalHeader, ModalBody, Container} from 'reactstrap';
+import TokenContext from '../../Contexts/TokenContext'
+import APIURL from '../../helpers/environment'
+
+const ReviewEdit = (props) => {
+    let tokenContext = React.useContext(TokenContext);
+
+    const [editRating, setEditRating] = useState(props.location.state.userRating);
+    const [editReview, setEditReview] = useState(props.location.state.userReview);
+
+    const reviewUpdate = (e, review) => {
+        e.preventDefault();
+        fetch(`${APIURL}/review/updatereview${props.reviewToUpdate.id}`,{
+            method: 'PUT',
+            body: JSON.stringify({update: {rating: editRating, review: editReview}}),
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'Authorization': tokenContext.token
+            })
+        }) .then((res) => {
+            props.fetchReviews();
+            props.updateOff();
+        })
+    }
+    return(
+        // <Modal isOpen={true}>
+        //     <ModalHeader>Edit a Review</ModalHeader>
+        //     <ModalBody>
+        //         <Form onSubmit={reviewUpdate}>
+        //             <FormGroup>
+        //                 <Label htmlFor="rating">Edit Rating:</Label>
+        //                 <Input name="rating" value={editRating} onChange={(e)=> setEditRating(e.target.value)}/>
+        //             </FormGroup>
+        //             <FormGroup>
+        //                 <Label htmlFor="review">Edit Review:</Label>
+        //                 <Input name="review" value={editReview} onChange={(e) => setEditReview(e.target.value)}/>
+        //             </FormGroup>
+        //             <Button type="submit">Update Review</Button>
+        //         </Form>
+        //     </ModalBody>
+        // </Modal>
+        <Container>
+            <h1>Edit Review for {props.location.state.gameName}</h1>
+            <Form onSubmit={reviewUpdate}>
+                <FormGroup>
+                    <Label htmlFor="rating">Edit Rating:</Label>
+                    <Input type="select" name="rating" value={editRating} onChange={(e)=> setEditRating(e.target.value)}>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </Input>
+                </FormGroup>
+                <FormGroup>
+                    <Label htmlFor="review">Edit Review:</Label>
+                    <Input name="review" value={editReview} onChange={(e) => setEditReview(e.target.value)}/>
+                </FormGroup>
+                <Button type="submit" color="dark">Submit</Button>
+            </Form>
+        </Container>
+    )
+}
+
+
+export default ReviewEdit;
